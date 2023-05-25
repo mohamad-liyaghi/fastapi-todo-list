52159dbdb713c27de5c605f3b4bff5f2da79e251
Mohamad Liyaghi <liaghimohamad69@gmail.com>
2023-05-25 19:39:46 +0330

    Factory class added

---
+++ src/core/factory/base.py
@@ -0,0 +1,10 @@
+class Factory:
+    """
+    This is the factory container that will instantiate all the controllers and
+    repositories which can be accessed by the rest of the application.
+    """
+
+    # Repositories
+    # TODO add repos here
+
+    # TODO implement get_controllers
